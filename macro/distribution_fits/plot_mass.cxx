723cd49f7f65eb9e572381d503d7388b39f95dac
sawankumawat <sawankumawat4@gmail.com>
2026-02-19 15:56:08 +0530

    added code for normalization range variaition plots for mixed-event

--- macro/distribution_fits/plot_mass.cxx
+++ macro/distribution_fits/plot_mass.cxx
@@ -97,17 +97,17 @@ void plot_mass()
     band1710Mass->SetLineWidth(1);
     band1710Mass->Draw("same");
     cout << "mass of f01710 in pdg is " << f1710Mass << " GeV/c2" << endl;
-    SetHistoQA(hIntegratedMass);
-    hIntegratedMass->SetMarkerStyle(21);
-    hIntegratedMass->SetMarkerColor(kGreen + 2);
-    hIntegratedMass->SetLineColor(kGreen + 2);
-    hIntegratedMass->SetMarkerSize(1.5);
-    hIntegratedMass->Draw("p same");
-    SetHistoQA(hIntegratedMassSys);
-    hIntegratedMassSys->SetLineColor(kGreen + 2);
-    hIntegratedMassSys->SetMarkerColor(kGreen + 2);
-    hIntegratedMassSys->SetFillStyle(0);
-    hIntegratedMassSys->Draw("e2 same");
+    // SetHistoQA(hIntegratedMass);
+    // hIntegratedMass->SetMarkerStyle(21);
+    // hIntegratedMass->SetMarkerColor(kGreen + 2);
+    // hIntegratedMass->SetLineColor(kGreen + 2);
+    // hIntegratedMass->SetMarkerSize(1.5);
+    // hIntegratedMass->Draw("p same");
+    // SetHistoQA(hIntegratedMassSys);
+    // hIntegratedMassSys->SetLineColor(kGreen + 2);
+    // hIntegratedMassSys->SetMarkerColor(kGreen + 2);
+    // hIntegratedMassSys->SetFillStyle(0);
+    // hIntegratedMassSys->Draw("e2 same");
     TLegend *leg1710Mass = new TLegend(0.45, 0.67, 0.9, 0.93);
     leg1710Mass->SetBorderSize(0);
     leg1710Mass->SetFillStyle(0);
@@ -116,14 +116,15 @@ void plot_mass()
     leg1710Mass->AddEntry((TObject *)0, "pp, #sqrt{#it{s}} = 13.6 TeV", "");
     leg1710Mass->AddEntry((TObject *)0, "FT0M: 0-100%, |y|<0.5", "");
     leg1710Mass->AddEntry(hMass1710, "f_{0}(1710)", "p");
-    leg1710Mass->AddEntry(hIntegratedMass, "f_{0}(1710) (integrated)", "p");
+    // leg1710Mass->AddEntry(hIntegratedMass, "f_{0}(1710) (integrated)", "p");
     // leg1710Mass->AddEntry(hMass1710, "This analysis", "pe");
     // leg1710Mass->AddEntry(line1710Mass, "PDG value", "l");
     band1710Mass->SetLineWidth(0);
     leg1710Mass->AddEntry(line1710Mass, "PDG value", "l");
     leg1710Mass->Draw();
     lat.DrawLatex(0.5, 0.2, "Uncertainties: stat.(bars), sys.(boxes)");
-    cMass1710->SaveAs((path + "/plots/Mass_f0_sys_compareIntegrated.png").c_str());
+    cMass1710->SaveAs((path + "/plots/Mass_f0_sys.png").c_str());
+    // cMass1710->SaveAs((path + "/plots/Mass_f0_sys_compareIntegrated.png").c_str());
 
     TCanvas *cMass1525 = new TCanvas("cMass1525", "Mass vs pT", 720, 720);
     SetCanvasStyle(cMass1525, 0.15, 0.03, 0.05, 0.13);
@@ -164,87 +165,87 @@ void plot_mass()
     leg1525Mass->AddEntry(line1525Mass, "PDG value", "l");
     lat.DrawLatex(0.5, 0.2, "Uncertainties: stat.(bars), sys.(boxes)");
     leg1525Mass->Draw();
-    // cMass1525->SaveAs((path + "/plots/Mass_f2_sys.png").c_str());
+    cMass1525->SaveAs((path + "/plots/Mass_f2_sys.png").c_str());
 
-    // Plot width and compare with pt-integrated values
-    TFile *fWidthFree = new TFile((path + "/../../FitParam_f0WidthFree2.root").c_str(), "READ");
-    if (fWidthFree->IsZombie())
-    {
-        cout << "File not found " << (path + "/../../FitParam_f0WidthFree2.root").c_str() << endl;
-        return;
-    }
-    TH1F *hWidth1710 = (TH1F *)fWidthFree->Get("Mult_0_100/hWidth_1710");
-    if (hWidth1710 == nullptr)
-    {
-        cout << "Width histogram not found" << endl;
-        return;
-    }
-    double f1710Width_integrated = 0.1456;
-    double f1710Width_StatErr = 0.0151;
-    double f1710Width_SysErr = 0.0419;
-    TH1F *hIntegratedWidth = new TH1F("hIntegratedWidth", "", 1, 0.003, 15);
-    hIntegratedWidth->SetBinContent(1, f1710Width_integrated);
-    hIntegratedWidth->SetBinError(1, f1710Width_StatErr);
-    TH1F *hIntegratedWidthSys = new TH1F("hIntegratedWidthSys", "", 1, 0.003, 15);
-    hIntegratedWidthSys->SetBinContent(1, f1710Width_integrated);
-    hIntegratedWidthSys->SetBinError(1, f1710Width_SysErr);
+    // // Plot width and compare with pt-integrated values
+    // TFile *fWidthFree = new TFile((path + "/../../FitParam_f0WidthFree2.root").c_str(), "READ");
+    // if (fWidthFree->IsZombie())
+    // {
+    //     cout << "File not found " << (path + "/../../FitParam_f0WidthFree2.root").c_str() << endl;
+    //     return;
+    // }
+    // TH1F *hWidth1710 = (TH1F *)fWidthFree->Get("Mult_0_100/hWidth_1710");
+    // if (hWidth1710 == nullptr)
+    // {
+    //     cout << "Width histogram not found" << endl;
+    //     return;
+    // }
+    // double f1710Width_integrated = 0.1456;
+    // double f1710Width_StatErr = 0.0151;
+    // double f1710Width_SysErr = 0.0419;
+    // TH1F *hIntegratedWidth = new TH1F("hIntegratedWidth", "", 1, 0.003, 15);
+    // hIntegratedWidth->SetBinContent(1, f1710Width_integrated);
+    // hIntegratedWidth->SetBinError(1, f1710Width_StatErr);
+    // TH1F *hIntegratedWidthSys = new TH1F("hIntegratedWidthSys", "", 1, 0.003, 15);
+    // hIntegratedWidthSys->SetBinContent(1, f1710Width_integrated);
+    // hIntegratedWidthSys->SetBinError(1, f1710Width_SysErr);
 
-    TCanvas *cWidth1710 = new TCanvas("cWidth1710", "Width vs pT", 720, 720);
-    SetCanvasStyle(cWidth1710, 0.15, 0.01, 0.05, 0.13);
-    TH1F *histDummy = new TH1F("histDummy", "", hMass1710->GetNbinsX(), hMass1710->GetXaxis()->GetXmin(), hMass1710->GetXaxis()->GetXmax());
-    SetHistoQA(histDummy);
-    histDummy->GetXaxis()->SetTitle("#it{p}_{T} (GeV/#it{c})");
-    histDummy->GetYaxis()->SetTitle("Width (GeV/#it{c}^{2})");
-    histDummy->GetYaxis()->SetTitleOffset(1.5);
-    histDummy->GetYaxis()->SetRangeUser(0.0, 0.409);
-    histDummy->Draw();
-    SetHistoQA(hWidth1710);
-    // hWidth1710->GetXaxis()->SetTitle("#it{p}_{T} (GeV/#it{c})");
-    // hWidth1710->GetYaxis()->SetTitle("Width (GeV/#it{c}^{2})");
-    // hWidth1710->GetYaxis()->SetTitleOffset(1.5);
-    // hWidth1710->GetYaxis()->SetRangeUser(0.0, 0.429);
-    hWidth1710->SetLineColor(kBlue);
-    hWidth1710->SetMarkerColor(kBlue);
-    hWidth1710->SetMarkerStyle(20);
-    hWidth1710->SetMarkerSize(1.5);
-    hWidth1710->GetXaxis()->SetRangeUser(0, 16);
-    hWidth1710->Draw("pe same");
-    TLine *line1710Width = new TLine(1, f1710Width, 15, f1710Width);
-    line1710Width->SetLineStyle(2);
-    line1710Width->SetLineColor(2);
-    line1710Width->Draw("same");
-    TBox *band1710Width = new TBox(1, f1710Width - f1710WidthErr, 15, f1710Width + f1710WidthErr);
-    band1710Width->SetFillStyle(3001);
-    band1710Width->SetFillColorAlpha(kRed, 0.2); // shaded
-    band1710Width->SetLineColor(kRed);
-    band1710Width->SetLineWidth(1);
-    band1710Width->Draw("same");
-    cout << "Width of f01710 in pdg is " << f1710Width << " GeV/c2" << endl;
-    SetHistoQA(hIntegratedWidth);
-    hIntegratedWidth->SetMarkerStyle(21);
-    hIntegratedWidth->SetMarkerColor(kGreen + 2);
-    hIntegratedWidth->SetLineColor(kGreen + 2);
-    hIntegratedWidth->SetMarkerSize(1.5);
-    hIntegratedWidth->Draw("p same");
-    SetHistoQA(hIntegratedWidthSys);
-    hIntegratedWidthSys->SetLineColor(kGreen + 2);
-    hIntegratedWidthSys->SetMarkerColor(kGreen + 2);
-    hIntegratedWidthSys->SetFillStyle(0);
-    hIntegratedWidthSys->Draw("e2 same");
-    TLegend *leg1710Width = new TLegend(0.45, 0.67, 0.9, 0.93);
-    leg1710Width->SetBorderSize(0);
-    leg1710Width->SetFillStyle(0);
-    leg1710Width->SetTextSize(0.035);
-    leg1710Width->AddEntry((TObject *)0, "ALICE", "");
-    leg1710Width->AddEntry((TObject *)0, "pp, #sqrt{#it{s}} = 13.6 TeV", "");
-    leg1710Width->AddEntry((TObject *)0, "FT0M: 0-100%, |y|<0.5", "");
-    leg1710Width->AddEntry(hWidth1710, "f_{0}(1710)", "p");
-    leg1710Width->AddEntry(hIntegratedWidth, "f_{0}(1710) (integrated)", "p");
-    // leg1710Width->AddEntry(hWidth1710, "This analysis", "pe");
+    // TCanvas *cWidth1710 = new TCanvas("cWidth1710", "Width vs pT", 720, 720);
+    // SetCanvasStyle(cWidth1710, 0.15, 0.01, 0.05, 0.13);
+    // TH1F *histDummy = new TH1F("histDummy", "", hMass1710->GetNbinsX(), hMass1710->GetXaxis()->GetXmin(), hMass1710->GetXaxis()->GetXmax());
+    // SetHistoQA(histDummy);
+    // histDummy->GetXaxis()->SetTitle("#it{p}_{T} (GeV/#it{c})");
+    // histDummy->GetYaxis()->SetTitle("Width (GeV/#it{c}^{2})");
+    // histDummy->GetYaxis()->SetTitleOffset(1.5);
+    // histDummy->GetYaxis()->SetRangeUser(0.0, 0.409);
+    // histDummy->Draw();
+    // SetHistoQA(hWidth1710);
+    // // hWidth1710->GetXaxis()->SetTitle("#it{p}_{T} (GeV/#it{c})");
+    // // hWidth1710->GetYaxis()->SetTitle("Width (GeV/#it{c}^{2})");
+    // // hWidth1710->GetYaxis()->SetTitleOffset(1.5);
+    // // hWidth1710->GetYaxis()->SetRangeUser(0.0, 0.429);
+    // hWidth1710->SetLineColor(kBlue);
+    // hWidth1710->SetMarkerColor(kBlue);
+    // hWidth1710->SetMarkerStyle(20);
+    // hWidth1710->SetMarkerSize(1.5);
+    // hWidth1710->GetXaxis()->SetRangeUser(0, 16);
+    // hWidth1710->Draw("pe same");
+    // TLine *line1710Width = new TLine(1, f1710Width, 15, f1710Width);
+    // line1710Width->SetLineStyle(2);
+    // line1710Width->SetLineColor(2);
+    // line1710Width->Draw("same");
+    // TBox *band1710Width = new TBox(1, f1710Width - f1710WidthErr, 15, f1710Width + f1710WidthErr);
+    // band1710Width->SetFillStyle(3001);
+    // band1710Width->SetFillColorAlpha(kRed, 0.2); // shaded
+    // band1710Width->SetLineColor(kRed);
+    // band1710Width->SetLineWidth(1);
+    // band1710Width->Draw("same");
+    // cout << "Width of f01710 in pdg is " << f1710Width << " GeV/c2" << endl;
+    // SetHistoQA(hIntegratedWidth);
+    // hIntegratedWidth->SetMarkerStyle(21);
+    // hIntegratedWidth->SetMarkerColor(kGreen + 2);
+    // hIntegratedWidth->SetLineColor(kGreen + 2);
+    // hIntegratedWidth->SetMarkerSize(1.5);
+    // hIntegratedWidth->Draw("p same");
+    // SetHistoQA(hIntegratedWidthSys);
+    // hIntegratedWidthSys->SetLineColor(kGreen + 2);
+    // hIntegratedWidthSys->SetMarkerColor(kGreen + 2);
+    // hIntegratedWidthSys->SetFillStyle(0);
+    // hIntegratedWidthSys->Draw("e2 same");
+    // TLegend *leg1710Width = new TLegend(0.45, 0.67, 0.9, 0.93);
+    // leg1710Width->SetBorderSize(0);
+    // leg1710Width->SetFillStyle(0);
+    // leg1710Width->SetTextSize(0.035);
+    // leg1710Width->AddEntry((TObject *)0, "ALICE", "");
+    // leg1710Width->AddEntry((TObject *)0, "pp, #sqrt{#it{s}} = 13.6 TeV", "");
+    // leg1710Width->AddEntry((TObject *)0, "FT0M: 0-100%, |y|<0.5", "");
+    // leg1710Width->AddEntry(hWidth1710, "f_{0}(1710)", "p");
+    // leg1710Width->AddEntry(hIntegratedWidth, "f_{0}(1710) (integrated)", "p");
+    // // leg1710Width->AddEntry(hWidth1710, "This analysis", "pe");
+    // // leg1710Width->AddEntry(line1710Width, "PDG value", "l");
+    // band1710Width->SetLineWidth(0);
     // leg1710Width->AddEntry(line1710Width, "PDG value", "l");
-    band1710Width->SetLineWidth(0);
-    leg1710Width->AddEntry(line1710Width, "PDG value", "l");
-    lat.DrawLatex(0.5, 0.2, "Uncertainties: stat.(bars), sys.(boxes)");
-    leg1710Width->Draw();
-    cWidth1710->SaveAs((path + "/plots/Width_f0_sys_compareIntegrated.png").c_str());
+    // lat.DrawLatex(0.5, 0.2, "Uncertainties: stat.(bars), sys.(boxes)");
+    // leg1710Width->Draw();
+    // cWidth1710->SaveAs((path + "/plots/Width_f0_sys_compareIntegrated.png").c_str());
 }
